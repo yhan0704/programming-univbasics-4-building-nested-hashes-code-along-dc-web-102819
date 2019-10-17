501d3579e97cde417fece5b6f508ec585248f785
pleasant-controller-2669 <han20@umbc.edu>
2019-10-17 17:11:25 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,8 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague",
-        :age => 53
+        :name : "Lord Montague",
+        :age : 53
         },
       :matriarch => {
         #your key/value pairs here
